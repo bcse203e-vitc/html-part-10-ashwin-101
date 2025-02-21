3a0237bf7a53a34aa7af809bc297ed5969e0018a
ashwin-101 <slathiaashwin2@gmail.com>
2025-02-21 12:21:10 +0530

    Add files via upload

---
+++ Assignment-10/q2.js
@@ -0,0 +1,7 @@
+document.getElementById('red-button').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('blue-button').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blue';
+});
